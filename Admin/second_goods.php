1722f4c93f830bfb5c1d9bd0844d94f4fe253785
Khatab420 <54971663+khatab420@users.noreply.github.com>
2023-07-31 09:16:22 +0430

    new changes
    new changes

--- Admin/second_goods.php
+++ Admin/second_goods.php
@@ -139,6 +139,21 @@ function addproduct()
             <li><a class="dropdown-item" href="#">Sign out</a></li>
           </ul>
         </div>
+        <div class="flex-shrink-0 dropdown">
+          <a href="#" class="d-block link-light text-decoration-none dropdown-toggle" id="dropdownUser2"
+            data-bs-toggle="dropdown" aria-expanded="false">
+            <img src="https://via.placeholder.com/28?text=!" alt="user" width="32" height="32" class="rounded-circle">
+          </a>
+          <ul class="dropdown-menu dropdown-menu-end shadow" aria-labelledby="dropdownUser2" style="">
+
+            <li><a class="dropdown-item" href="#">Settings</a></li>
+            <li><a class="dropdown-item" href="#">Profile</a></li>
+            <li>
+              <hr class="dropdown-divider">
+            </li>
+            <li><a class="dropdown-item" href="#">Sign out</a></li>
+          </ul>
+        </div>
       </div>
     </div>
   </header>
